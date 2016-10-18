4ea6477b3b2df70b00a18eba18d8c69681cd2abe
manuelRod <manuel.rodriguez.rosado@gmail.com>
2016-10-18 10:54:28 +0000

    basic plugin setup

---
+++ tansact-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: WP transact.io
+ * Description: Integrates transact.io services into WP
+ * Version: 0.0.1
+ * Author: transact.io
+ * Author URI: http://www.transact.io
+ */
+
+namespace Transact;
