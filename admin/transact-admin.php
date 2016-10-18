8a2d828a0ce81f1a0a25ed7e04fcd14410fac939
manuelRod <manuel.rodriguez.rosado@gmail.com>
2016-10-18 13:51:20 +0000

    dashboard menu

--- admin/transact-admin.php
+++ admin/transact-admin.php
@@ -11,6 +11,24 @@ class DashboardExtension
      */
     public function hookToDashboard()
     {
+        add_action( 'admin_menu', array($this, 'add_transact_menu' ));
 
     }
+
+    public function add_transact_menu()
+    {
+        add_menu_page( 'transact.io', 'transact.io', 'manage_options', 'transact-admin-page.php', array($this, 'transact_io_admin_callback'), 'dashicons-cart' );
+    }
+
+    public function transact_io_admin_callback()
+    {
+        ?>
+        <div class="wrap">
+            <h2>transact.io Account</h2>
+        </div>
+        <?php
+    }
+
+
 }
+
