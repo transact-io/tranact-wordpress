048c18860cbe2cb068bf75554ae6fab8b8e62fef
manuelRod <manuel.rodriguez.rosado@gmail.com>
2016-10-25 16:03:59 +0000

    transact premium

---
+++ frontend/controllers/transact-api.php
@@ -0,0 +1,16 @@
+<?php
+namespace Transact\FrontEnd\Controllers\Api;
+
+/**
+ * Class TransactApi
+ */
+class TransactApi
+{
+
+    public function is_premium() {
+        return false;
+    }
+
+
+
+}
--- frontend/controllers/transact-single-post.php
+++ frontend/controllers/transact-single-post.php
@@ -1,6 +1,10 @@
 <?php
 namespace Transact\FrontEnd\Controllers\Post;
 
+use Transact\FrontEnd\Controllers\Api\TransactApi;
+require_once  plugin_dir_path(__FILE__) . '/transact-api.php';
+
+
 /**
  * Class FrontEndPostExtension
  */
@@ -11,7 +15,24 @@ class FrontEndPostExtension
      */
     public function hookSinglePost()
     {
-
+        add_filter( 'the_content', array($this, 'filter_pre_get_content' ));
     }
 
+    /**
+     * Hooks into content, if the user is premium for that content
+     * it will show the premium content for it, otherwise the normal one.
+     *
+     * @param string $content
+     * @return string
+     */
+    public function filter_pre_get_content($content)
+    {
+        if ((new TransactApi())->is_premium())
+        {
+            $premium_content = get_post_meta( get_the_ID(), 'transact_premium_content' , true ) ;
+            return $premium_content;
+        }
+
+        return $content;
+    }
 }
