46c79a358188c84ee14bd9f513e8903b838f9ce9
manuelRod <manuel.rodriguez.rosado@gmail.com>
2017-02-06 15:37:05 +0000

    adding new subscription table

--- activation.php
+++ activation.php
@@ -12,3 +12,12 @@ $sql        = "CREATE TABLE IF NOT EXISTS {$table_name} (
 
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 dbDelta($sql);
+
+$subscription_table = $wpdb->prefix . 'transact_subscription_transactions';
+$sql        = "CREATE TABLE IF NOT EXISTS {$subscription_table} (
+          `sales_id` varchar(32) NOT NULL,
+          `expiration` bigint(20) NOT NULL,
+          `timestamp` bigint(20) NOT NULL,
+          PRIMARY KEY (`sales_id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+dbDelta($sql);
