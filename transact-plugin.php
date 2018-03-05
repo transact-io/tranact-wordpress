409288e4eedc05d24644df5c2e2078c5d4a7111d
Karl Hiramoto <karl@xsact.com>
2018-03-05 21:41:28 +0100

    version bump

--- transact-plugin.php
+++ transact-plugin.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: transact.io
  * Description: Integrates transact.io services into WP
- * Version: 1.6.0
+ * Version: 1.7.0
  * Author: transact.io
  * Author URI: https://transact.io
  * Plugin URI: https://wordpress.org/plugins/transact/
